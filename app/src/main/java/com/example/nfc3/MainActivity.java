7d49cb4c3097f43a7de1fcffff58cffbf67bcaf8
Cyrine Aouadhi <contact.cyrin@gmail.com>
2023-07-07 16:05:19 +0200

    Send/Receive

--- app/src/main/java/com/example/nfc3/MainActivity.java
+++ app/src/main/java/com/example/nfc3/MainActivity.java
@@ -2,53 +2,132 @@ package com.example.nfc3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
-import android.nfc.cardemulation.CardEmulation;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
-
-import java.util.List;
+import android.provider.Settings;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String PAYMENT_CATEGORY = "android.intent.category.DEFAULT";
     private static final String PAYMENT_ACTION = "android.intent.action.PAY";
 
+    private boolean isNfcEnabled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        Intent serviceIntent = new Intent(this, MyHostApduService.class);
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Check if NFC is enabled
+        if (!isNfcEnabled) {
+            isNfcEnabled = checkNfcEnabled();
+            if (!isNfcEnabled) {
+                // NFC is not enabled, redirect the user to NFC settings
+                Intent settingsIntent = new Intent(Settings.ACTION_NFC_SETTINGS);
+                startActivity(settingsIntent);
+                return; // Finish the activity to prevent further execution
+            }
+        }
+//
+//        Intent serviceIntent = new Intent(this, MyHostApduService.class);
+//        startService(serviceIntent);
+
+//
+        Intent serviceIntent = new Intent(this, sendAdpeService.class);
         startService(serviceIntent);
+
         if (isDefaultPaymentApp()) {
             // Your app is recognized as a contactless payment app
             // Add your logic here
-            int k=5;
+            Toast.makeText(this, "Your app is recognized as a contactless payment app", Toast.LENGTH_SHORT).show();
         }
     }
 
+    private boolean checkNfcEnabled() {
+        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        return nfcAdapter != null && nfcAdapter.isEnabled();
+    }
 
     private boolean isDefaultPaymentApp() {
         PackageManager packageManager = getPackageManager();
-
-        // Create an intent with payment action and category
         Intent paymentIntent = new Intent(PAYMENT_ACTION);
         paymentIntent.addCategory(PAYMENT_CATEGORY);
 
-        // Resolve the intent to get the list of activities that handle it
-        List<ResolveInfo> resolveInfos = packageManager.queryIntentActivities(paymentIntent, 0);
-
-        // Check if any activity in your app handles the payment intent
-        for (ResolveInfo resolveInfo : resolveInfos) {
-            if (resolveInfo.activityInfo.packageName.equals(getPackageName())) {
-                return true; // App is registered as a default payment app
-            }
-        }
-
-        return false; // App is not registered as a default payment app
+        ResolveInfo resolveInfo = packageManager.resolveActivity(paymentIntent, PackageManager.MATCH_DEFAULT_ONLY);
+        return resolveInfo != null && resolveInfo.activityInfo.packageName.equals(getPackageName());
     }
 
+
+
+
+
+
 }
+
+
+
+//package com.example.nfc3;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.content.ComponentName;
+//import android.content.Intent;
+//import android.content.pm.PackageManager;
+//import android.content.pm.ResolveInfo;
+//import android.nfc.cardemulation.CardEmulation;
+//import android.os.Bundle;
+//
+//import java.util.List;
+//
+//public class MainActivity extends AppCompatActivity {
+//    private static final String PAYMENT_CATEGORY = "android.intent.category.DEFAULT";
+//    private static final String PAYMENT_ACTION = "android.intent.action.PAY";
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//
+//
+//
+//        Intent serviceIntent = new Intent(this, MyHostApduService.class);
+//        startService(serviceIntent);
+//        if (isDefaultPaymentApp()) {
+//            // Your app is recognized as a contactless payment app
+//            // Add your logic here
+//            int k=5;
+//        }
+//    }
+//
+//
+//    private boolean isDefaultPaymentApp() {
+//        PackageManager packageManager = getPackageManager();
+//
+//        // Create an intent with payment action and category
+//        Intent paymentIntent = new Intent(PAYMENT_ACTION);
+//        paymentIntent.addCategory(PAYMENT_CATEGORY);
+//
+//        // Resolve the intent to get the list of activities that handle it
+//        List<ResolveInfo> resolveInfos = packageManager.queryIntentActivities(paymentIntent, 0);
+//
+//        // Check if any activity in your app handles the payment intent
+//        for (ResolveInfo resolveInfo : resolveInfos) {
+//            if (resolveInfo.activityInfo.packageName.equals(getPackageName())) {
+//                return true; // App is registered as a default payment app
+//            }
+//        }
+//
+//        return false; // App is not registered as a default payment app
+//    }
+//
+//}
--- app/src/main/java/com/example/nfc3/MyHostApduService.java
+++ app/src/main/java/com/example/nfc3/MyHostApduService.java
@@ -11,29 +11,50 @@ public class MyHostApduService extends HostApduService {
     private static final String SELECT_APDU_HEADER = "00A40400";
     private static final String SAMPLE_APP_AID = "F000000001";
 
-    @Override
+    private static final String HEX_STRING = "6F2D840E325041592E5359532E4444463031A51BBF0C1861164F07A0000000031010500B56495341204352454449549000";
+//    @Override
+//    public byte[] processCommandApdu(byte[] commandApdu, Bundle extras) {
+//
+//        byte[] byteArrayToSend = hexStringToByteArray(HEX_STRING);
+//
+//        //  logic to send the byteArrayToSend
+//        StringBuilder stringBuilder = new StringBuilder();
+//        for (byte b : byteArrayToSend) {
+//            stringBuilder.append(b & 0xFF).append(" sended APDU");
+//        }
+//        Log.d(TAG, stringBuilder.toString());
+//
+//
+//        if (commandApdu == null) {
+//            return byteArrayToSend;
+//        }
+//
+//        String command = ByteArrayToHexString(commandApdu);
+//
+//        Log.d(TAG, "Received APDU: " + command);
+//
+//        if (command.startsWith(SELECT_APDU_HEADER)) {
+//            if (command.endsWith(SAMPLE_APP_AID)) {
+//                byte[] response = "OK".getBytes();
+//                return ConcatArrays(response, HexStringToByteArray("9000"));
+//            } else {
+//                byte[] response = "ERROR".getBytes();
+//                return ConcatArrays(response, HexStringToByteArray("6A82"));
+//            }
+//        } else {
+//            byte[] response = "UNKNOWN".getBytes();
+//            return ConcatArrays(response, HexStringToByteArray("6D00"));
+//        }
+//    }
+
+
     public byte[] processCommandApdu(byte[] commandApdu, Bundle extras) {
-        if (commandApdu == null) {
-            return new byte[0];
-        }
+        byte[] byteArrayToSend = hexStringToByteArray(HEX_STRING);
+        Log.d(TAG, "Received APDU: " + ByteUtils.byteArray2HexString(commandApdu));
 
-        String command = ByteArrayToHexString(commandApdu);
-
-        Log.d(TAG, "Received APDU: " + command);
-
-        if (command.startsWith(SELECT_APDU_HEADER)) {
-            if (command.endsWith(SAMPLE_APP_AID)) {
-                byte[] response = "OK".getBytes();
-                return ConcatArrays(response, HexStringToByteArray("9000"));
-            } else {
-                byte[] response = "ERROR".getBytes();
-                return ConcatArrays(response, HexStringToByteArray("6A82"));
-            }
-        } else {
-            byte[] response = "UNKNOWN".getBytes();
-            return ConcatArrays(response, HexStringToByteArray("6D00"));
-        }
+        return byteArrayToSend;
     }
+
 
     private String ByteArrayToHexString(byte[] bytes) {
         StringBuilder sb = new StringBuilder(bytes.length * 2);
@@ -43,24 +64,39 @@ public class MyHostApduService extends HostApduService {
         return sb.toString();
     }
 
-    private byte[] HexStringToByteArray(String hexString) {
-        int length = hexString.length() / 2;
-        byte[] bytes = new byte[length];
-        for (int i = 0; i < length; i++) {
-            bytes[i] = (byte) Integer.parseInt(hexString.substring(2 * i, 2 * i + 2), 16);
+
+
+    private String byteArrayToHexString(byte[] byteArray) {
+        StringBuilder stringBuilder = new StringBuilder(byteArray.length * 2);
+        for (byte b : byteArray) {
+            stringBuilder.append(String.format("%02X", b));
         }
-        return bytes;
+        return stringBuilder.toString();
     }
 
-    private byte[] ConcatArrays(byte[] firstArray, byte[] secondArray) {
-        byte[] result = new byte[firstArray.length + secondArray.length];
-        System.arraycopy(firstArray, 0, result, 0, firstArray.length);
-        System.arraycopy(secondArray, 0, result, firstArray.length, secondArray.length);
-        return result;
+    private byte[] concatArrays(byte[] array1, byte[] array2) {
+        byte[] concatenatedArray = new byte[array1.length + array2.length];
+        System.arraycopy(array1, 0, concatenatedArray, 0, array1.length);
+        System.arraycopy(array2, 0, concatenatedArray, array1.length, array2.length);
+        return concatenatedArray;
     }
+
+
 
     @Override
     public void onDeactivated(int reason) {
         Log.d(TAG, "Deactivated: " + reason);
     }
+
+
+    private byte[] hexStringToByteArray(String hexString) {
+
+        byte[] byteArray = ByteUtils.hexString2ByteArray(hexString);
+
+
+        return byteArray;
+    }
+
+
+
 }
